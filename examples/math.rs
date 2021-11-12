3e205ff06307e883c8b789980f5b6b1065e31798
Robin Mueller <robin.mueller.m@gmail.com>
2021-11-12 01:12:08 +0100

    added math file

---
+++ examples/math.rs
@@ -0,0 +1,32 @@
+//! Some mathematical functions like sin or sqrt are not available
+//! with #![no_std]. An external library like libm can be used to still
+//! perform advanced calculations
+
+#![no_main]
+#![no_std]
+
+use panic_halt as _;
+
+use cortex_m_rt::entry;
+use cortex_m_semihosting::hprintln;
+use libm::{sqrtf, floorf, sin, exp};
+
+#[entry]
+fn main() -> ! {
+
+    let float = 4.82832;
+    let floored_float = floorf(float);
+
+    let sqrt_of_four = sqrtf(floored_float);
+
+    let sinus_of_four = sin(floored_float.into());
+
+    let exponential_of_four = exp(floored_float.into());
+    hprintln!("Floored test float {} to {}", float, floored_float).unwrap();
+    hprintln!("The squareroot of {} is {}", floored_float, sqrt_of_four).unwrap();
+    hprintln!("The sinus of four is {}", sinus_of_four).unwrap();
+    hprintln!("The exponential of four to the base e is {}", exponential_of_four).unwrap();
+    loop {
+
+    }
+}
