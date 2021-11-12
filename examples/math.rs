dcb3507d7cb43e4ae0de8b1de310877671f98a43
Robin Mueller <robin.mueller.m@gmail.com>
2021-11-12 01:31:13 +0100

    update math example

--- examples/math.rs
+++ examples/math.rs
@@ -9,11 +9,10 @@ use panic_halt as _;
 
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
-use libm::{sqrtf, floorf, sin, exp};
+use libm::{exp, floorf, sin, sqrtf};
 
 #[entry]
 fn main() -> ! {
-
     let float = 4.82832;
     let floored_float = floorf(float);
 
@@ -23,10 +22,12 @@ fn main() -> ! {
 
     let exponential_of_four = exp(floored_float.into());
     hprintln!("Floored test float {} to {}", float, floored_float).unwrap();
-    hprintln!("The squareroot of {} is {}", floored_float, sqrt_of_four).unwrap();
+    hprintln!("The square root of {} is {}", floored_float, sqrt_of_four).unwrap();
     hprintln!("The sinus of four is {}", sinus_of_four).unwrap();
-    hprintln!("The exponential of four to the base e is {}", exponential_of_four).unwrap();
-    loop {
-
-    }
+    hprintln!(
+        "The exponential of four to the base e is {}",
+        exponential_of_four
+    )
+    .unwrap();
+    loop {}
 }
